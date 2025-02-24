cd647eec067c9181fe698a7a7b5914ad30f61b50
QasemAL <129743072+QasemAL@users.noreply.github.com>
2025-02-24 22:41:36 +0300

    WorldGeneration

--- Assets/WorldGenerator.cs
+++ Assets/WorldGenerator.cs
@@ -1,29 +1,39 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour
 {
-    public GameObject buildingPrefab;
-    public GameObject npcPrefab;
+    public List<GameObject> buildingPrefabs;
+    public List<GameObject> npcPrefabs; 
 
     public int buildingCount = 5;
     public int npcCount = 5;
-    public float spacing = 2.0f; 
+
+    public float buildingSpacing = 2.0f;
+    public float npcSpacing = 2.0f;
 
     public Vector2 buildingStartPosition = new Vector2(-5, 0); 
     public Vector2 npcStartPosition = new Vector2(-5, -2); 
 
     private void Start()
     {
-        SpawnObjects(buildingPrefab, buildingStartPosition, buildingCount);
-        SpawnObjects(npcPrefab, npcStartPosition, npcCount);
+        SpawnObjects(buildingPrefabs, buildingStartPosition, buildingCount, buildingSpacing);
+        SpawnObjects(npcPrefabs, npcStartPosition, npcCount, npcSpacing);
     }
 
-    private void SpawnObjects(GameObject prefab, Vector2 startPos, int count)
+    private void SpawnObjects(List<GameObject> prefabs, Vector2 startPos, int count, float spacing)
     {
+        if (prefabs.Count == 0)
+        {
+            Debug.LogWarning("No prefabs assigned in the list!");
+            return;
+        }
+
         for (int i = 0; i < count; i++)
         {
             Vector2 spawnPos = startPos + new Vector2(i * spacing, 0);
-            Instantiate(prefab, spawnPos, Quaternion.identity);
+            GameObject prefabToSpawn = prefabs[Random.Range(0, prefabs.Count)]; 
+            Instantiate(prefabToSpawn, spawnPos, Quaternion.identity);
         }
     }
 
@@ -31,10 +41,12 @@ public class WorldGenerator : MonoBehaviour
     {
         Gizmos.color = Color.green;
 
-        Vector2 buildingEnd = buildingStartPosition + new Vector2((buildingCount - 1) * spacing, 0);
+        Vector2 buildingEnd = buildingStartPosition + new Vector2((buildingCount - 1) * buildingSpacing, 0);
         Gizmos.DrawLine(buildingStartPosition, buildingEnd);
 
-        Vector2 npcEnd = npcStartPosition + new Vector2((npcCount - 1) * spacing, 0);
+        Gizmos.color = Color.red;
+
+        Vector2 npcEnd = npcStartPosition + new Vector2((npcCount - 1) * npcSpacing, 0);
         Gizmos.DrawLine(npcStartPosition, npcEnd);
     }
 }
