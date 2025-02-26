0a40af5639fab39b379bb428de924a10a21df7df
QasemAL <129743072+QasemAL@users.noreply.github.com>
2025-02-26 15:27:24 +0300

    AddedMoney

--- Assets/GameManger.cs
+++ Assets/GameManger.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 
 public class GameManger : MonoBehaviour
 {
-    public static GameManger Instance; // Singleton instance for easy access
+    public static GameManger Instance; 
 
     public List<GameObject> Buildings; // List of objects that will change sprites
+    public int Money = 0;
 
     void Awake()
     {
