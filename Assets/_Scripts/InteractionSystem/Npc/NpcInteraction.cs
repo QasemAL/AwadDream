373a5d3608072d9e4cf9131dc21369f2baabbf97
QasemAL <129743072+QasemAL@users.noreply.github.com>
2025-02-26 18:27:25 +0300

    AddedVoices

--- Assets/_Scripts/InteractionSystem/Npc/NpcInteraction.cs
+++ Assets/_Scripts/InteractionSystem/Npc/NpcInteraction.cs
@@ -1,4 +1,4 @@
-using UnityEditor.ShaderGraph.Serialization;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NpcInteraction : MonoBehaviour, IInteractable
@@ -6,7 +6,12 @@ public class NpcInteraction : MonoBehaviour, IInteractable
     public bool canInteractWith = false;
     public float MaxTargetTripRange = 30f;
     public GameObject PointPreFab;
-    public LayerMask wallLayer; 
+    public LayerMask wallLayer;
+    public List<AudioClip> audioClips;
+    public AudioSource audioSource;
+    [Range(0f, 1f)] public float audioPlayChance = 0.5f; // 50% chance to play audio instantly
+    public float minDelay = 1f;
+    public float maxDelay = 3f;
 
     public void Interact()
     {
@@ -14,7 +19,7 @@ public class NpcInteraction : MonoBehaviour, IInteractable
         player.GetComponent<PlayerInteraction>().GotPassenger = true;
 
         Vector2 spawnPosition = Vector2.zero;
-        int maxAttempts = 10; 
+        int maxAttempts = 10;
         bool validPosition = false;
 
         for (int i = 0; i < maxAttempts; i++)
@@ -24,7 +29,7 @@ public class NpcInteraction : MonoBehaviour, IInteractable
 
             RaycastHit2D hit = Physics2D.Raycast(transform.position, spawnPosition - (Vector2)transform.position, Vector2.Distance(transform.position, spawnPosition), wallLayer);
 
-            if (!hit.collider) 
+            if (!hit.collider)
             {
                 validPosition = true;
                 break;
@@ -34,7 +39,7 @@ public class NpcInteraction : MonoBehaviour, IInteractable
         if (!validPosition)
         {
             Debug.LogWarning("No valid trip point found!");
-            return; 
+            return;
         }
 
         var point = Instantiate(PointPreFab, spawnPosition, Quaternion.identity);
@@ -42,6 +47,27 @@ public class NpcInteraction : MonoBehaviour, IInteractable
         trip.TripMoney = Random.Range(4, 21);
         trip.passenger = this.gameObject;
 
+        // Play audio instantly OR after a delay
+        if (Random.value <= 0.5f)
+        {
+            PlayRandomAudio(); // Play immediately
+        }
+        else
+        {
+            float delay = Random.Range(minDelay, maxDelay);
+            Invoke(nameof(PlayRandomAudio), delay); // Play after a delay
+        }
+
         this.gameObject.SetActive(false);
+    }
+
+    private void PlayRandomAudio()
+    {
+        if (audioClips.Count > 0)
+        {
+            int index = Random.Range(0, audioClips.Count);
+            audioSource.PlayOneShot(audioClips[index]);
+            audioClips.RemoveAt(index); // Prevent replaying
+        }
     }
 }
