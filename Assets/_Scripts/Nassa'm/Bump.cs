b9c212506e2ec9c77fcfc9c21f59f5b229136a81
Nassam123 <130648214+Nassam123@users.noreply.github.com>
2025-02-26 18:27:03 +0300

    Update Bump.cs

--- Assets/_Scripts/Nassa'm/Bump.cs
+++ Assets/_Scripts/Nassa'm/Bump.cs
@@ -3,21 +3,23 @@ using UnityEngine;
 public class Bump : MonoBehaviour
 {
     public float bumpForce = 2f; // Force applied when hitting a bump
-    public float jumpForceOnBump = 10f; // Big jump force if the player doesn't slow down
+    public float jumpForceOnBump = 5f; // Big jump force if the player doesn't slow down
     public int penaltyAmount = 5; // Money lost for not slowing down
 
     private PlayerController playerController;
+    private CarVibration carVibration;
 
     void Start()
     {
         playerController = FindFirstObjectByType<PlayerController>();
+        carVibration = FindFirstObjectByType<CarVibration>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            if (playerController.GetCurrentSpeed() > playerController.moveSpeed * 0.5f) // If the player is going too fast
+            if (playerController.GetCurrentSpeed() > playerController.moveSpeed * 0.7f) // If the player is going too fast
             {
                 ApplyBigJumpEffect();
                 GameManger.Instance.LoseMoney(penaltyAmount); // Lose money for not slowing down
@@ -31,6 +33,7 @@ public class Bump : MonoBehaviour
 
     private void ApplyBumpEffect()
     {
+        carVibration.ApplyBumpEffectImg(bumpForce);
         // Small bump effect
         Rigidbody2D playerRb = playerController.GetComponent<Rigidbody2D>();
         if (playerRb != null)
@@ -38,6 +41,7 @@ public class Bump : MonoBehaviour
             playerRb.AddForce(Vector2.up * bumpForce, ForceMode2D.Impulse); // Bounce effect
            
         }
+
     }
 
     private void ApplyBigJumpEffect()
